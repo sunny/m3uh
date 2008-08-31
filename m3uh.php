30d3c6314f89b8c97656891008343f3802598eaa
Sunny <negatif@gmail.com>
2008-08-31 04:09:28 +0200

    Total refactorization!
    - no more ugly global array
    - uses constants
    - preg_replace wherever ereg_replace was
    - str_replace wherever ereg_replace wasn't needed
    - two-spaces indentation
    - removed unecessary comments
    - added comments and examples in the options
    - added seperation comments
    - renamed variables into simpler names
    - added check for file type
    - added ability to serve local files

--- m3uh.php
+++ m3uh.php
@@ -1,47 +1,66 @@
 <?php
 /*
-M3uh, an m3u music playlist generator in PHP
-Author: Sunny Ripert -- http://sunfox.org
-Licence: GPL -- http://www.gnu.org/copyleft/gpl.html
-*/
- 
-// options
-$dir = '../music/.';
-$dirloc = 'http://'.$_SERVER['HTTP_HOST'].'/music/';
- 
-// ugly recursive global array filler
-$allfiles = array();
-function recur_dir($dir) {
-    global $allfiles;
-    $dirlist = opendir($dir);
-    while ($file = readdir ($dirlist)) {
-        if ($file != '.' && $file != '..') {
-            $newpath = $dir.'/'.$file;
-            if (is_dir($newpath))
-                recur_dir($newpath); // recurse!
-            else 
-                $allfiles[] = rawurlencode($newpath);
-        }
- 
-    }
-    closedir($dirlist);
+ * m3uh, a m3u music playlist generator
+ * Author: Sunny Ripert -- http://sunfox.org
+ * Licence: GPL -- http://www.gnu.org/copyleft/gpl.html
+ */
+
+
+/* Options */
+// Path to the music folder
+define('PATH', '../music/');
+
+// You may add a prefix to add to link to your music files
+// may be a hard path or a uri, for example:
+# define('PREFIX', PATH);
+# define('PREFIX', '/home/sunny/music/');
+define('PREFIX', 'http://'.$_SERVER['HTTP_HOST'].'/music/');
+
+
+/* Configuration */
+define('EXTENSIONS_RE', '/\.(mp3|ogg|flac)$/');
+define('IS_URI', preg_match('/:\/\//', PREFIX));
+
+
+/* Functions */
+// Returns a list of all files in a directory recursively
+function files($dir, &$files = array()) {
+  $handle = opendir($dir);
+  while ($file = readdir($handle)) {
+    if ($file == '.' or $file == '..')
+      continue;
+
+    $path = $dir.'/'.$file;
+    if (is_dir($path))
+      files($path, $files);
+    else
+      $files[] = $path;
+  }
+  closedir($handle);
+  return $files;
 }
- 
- 
-// pile it up
-recur_dir($dir);
-natcasesort($allfiles);
- 
-// go!
+
+
+/* Go! */
+$i = 0;
+$files = files(PATH);
+natcasesort($files);
+
 header('Content-type: audio/x-mpegurl');
-echo "#EXTM3U\n"; // start of m3u
-for ($i = 0; $i < count($allfiles); $i++) {
-	$file = eregi_replace('%2F', '/', $allfiles[$i]); // put back the slashes
-	$file = substr($file, strlen($dir) + 1); // takes off awkward first dir and trailing slash
-	$name = eregi_replace('.mp3', '', rawurldecode(array_pop(explode('/', $file)))); // the name is last part of url, without .mp3
- 
-	echo "#EXTINF:$i,$name\n$dirloc$file\n";
+echo "#EXTM3U\n";
+foreach ($files as $file) {
+  if (!preg_match(EXTENSIONS_RE, $file))
+    continue;
+
+  ++$i;
+
+  $name = array_pop(explode('/', $file)); // the name is the last part of the filename
+	$name = preg_replace(EXTENSIONS_RE, '', $name);
+
+  $uri = substr($file, strlen(PATH) + 1); // take off the top path
+  $uri = IS_URI ? PREFIX . str_replace('%2F', '/', rawurlencode($uri)) : PREFIX . $uri;
+
+  echo "#EXTINF:$i,$name\n$uri\n";
 }
  
-?>
- 
+
